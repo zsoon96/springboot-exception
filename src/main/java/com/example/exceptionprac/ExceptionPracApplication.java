24577217ce8095dcecfb0b9d224c5e3ea4379e06
zsoon96 <pres0616@gmail.com>
2022-08-08 19:47:39 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/exceptionprac/ExceptionPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionPracApplication.class, args);
+	}
+
+}
