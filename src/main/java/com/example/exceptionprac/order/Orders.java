f51a715e9fa0803b25f1803cd445f5c13b49f798
zsoon96 <pres0616@gmail.com>
2022-08-14 21:35:40 +0900

    Order 예약어로 인한 주문 클래스명 수정(리팩터링)

---
+++ src/main/java/com/example/exceptionprac/order/Orders.java
@@ -0,0 +1,37 @@
+package com.example.exceptionprac.order;
+
+import com.example.exceptionprac.coupon.Coupon;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "price")
+    private double price;
+
+    // OneToOne일 때, 연관관계 주인은 OneToMany로 뱐경될 가능성이 있다고 판단되는 엔티티로 설정하는게 좋음
+    @OneToOne
+    @JoinColumn(name = "coupon_id" , referencedColumnName = "id", nullable = false)
+    private Coupon coupon;
+
+    @Builder
+    public Orders(double price) {
+        this.price = price;
+    }
+
+    public void applyCoupon ( Coupon coupon ) {
+        this.coupon = coupon;
+        coupon.use(this); // 쿠폰 사용 처리
+        price -= coupon.getDiscountAmount(); // 할인 가격 산출
+    }
+}
